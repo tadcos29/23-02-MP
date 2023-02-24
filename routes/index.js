56bb1d777d605a2ab9ce39567369fefb3e83aca7
tadcos29 <taddeo.costanzo@alumni.utoronto.ca>
2023-02-23 21:45:00 -0500

    Tweak to default send

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+
+router.get('/', async (req, res) => {
+    try {
+      res.status(200).json(`Use /api/travellers, /api/locations, /api/trips with their various associated GET, POST, and DELETE methods.`);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
